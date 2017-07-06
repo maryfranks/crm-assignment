4f88d4df8b8facaa197f6ffa81943f3bc871107b
maryfranks <maryfranks@gmail.com>
2017-07-06 16:09:17 -0400

    adds delete instance method to contacts.rb

--- contact.rb
+++ contact.rb
@@ -93,7 +93,7 @@ class Contact
   # This method should delete the contact
   # HINT: Check the Array class docs for built-in methods that might be useful here
   def delete
-
+    @@contacts.delete(self)
   end
 
   # Feel free to add other methods here, if you need them.
