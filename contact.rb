59febb2189f2820b0ca4d4fe464929a1b379f026
maryfranks <maryfranks@gmail.com>
2017-07-10 13:20:10 -0400

    adds active record and mini record to project + gemfiles

--- contact.rb
+++ contact.rb
@@ -1,3 +1,9 @@
+gem 'activerecord', '=4.2.9'
+require 'active_record'
+require 'mini_record'
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm.sqlite3')
+
 class Contact
 
   attr_accessor :first_name, :last_name, :email, :note
