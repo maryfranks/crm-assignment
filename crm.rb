a8ed905cc20e56d7f684f25d41ae49d51426cb18
maryfranks <maryfranks@gmail.com>
2017-07-10 14:15:03 -0400

    updates Contact.create in crm.rb

--- crm.rb
+++ crm.rb
@@ -50,7 +50,12 @@ class CRM
     print "Enter a Note: "
     note = gets.chomp
 
-    Contact.create(first_name, last_name, email, note)
+    contact = Contact.create(
+      first_name: first_name,
+      last_name:  last_name,
+      email:      email,
+      note:       note
+    )
   end
 
   def modify_existing_contact
